952824ed7f82c52d5457933441c66f800574b4f6
WoutKnaepenPXl <Wout.knaepen@student.pxl.be>
2026-02-12 14:46:10 +0100

    Tekstkleur aangepast naar cayan

--- ClonedProject/ClonedProject/Program.cs
+++ ClonedProject/ClonedProject/Program.cs
@@ -4,7 +4,10 @@
     {
         static void Main(string[] args)
         {
+            Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("Proficiat, je hebt je eerste clone gemaakt!");
+            Console.ResetColor();
+
         }
     }
 }
